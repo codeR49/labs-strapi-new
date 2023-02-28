376859a9c9c8ab2cb2d694a457872784bbe51268
codeR49 <arnabphukon.93@outlook.com>
2023-02-28 17:57:19 +0530

    config

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '15.207.110.45'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'trainsoft'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', 'Apurv@1998'),
-      ssl: env.bool('DATABASE_SSL', false),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
+      ssl: env.bool('DATABASE_SSL'),
     },
   },
 });
